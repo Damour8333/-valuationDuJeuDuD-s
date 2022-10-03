d17d1e3976bcc0613fc998ba131fe997f3555584
Damour8333 <98494743+Damour8333@users.noreply.github.com>
2022-10-03 10:52:37 +0200

    creation des const

--- README.md
+++ README.md
@@ -6,8 +6,27 @@ git branch -M main
 git remote add origin https://github.com/Damour8333/evaluationJavascriptStudyJeuDuDes.git
 git push -u origin main
 
-
+1/création du html
+2/création du nom et des variables (partie de création des noms en français en vue de l'évaluation)
 1/initialisé les variables
 2/ajouté les fonctions
 2/pouvoir entrez le nom de chaque participant
 3/stocker les boutons dans document.querySelector
+4/ajouté les fonctions pour les boutons
+5/ajouté les fonctions pour les dés
+6/ajouté les fonctions pour les scores
+7/ajouté le random  au bouton pour random les images
+8/ajouter le score  a chaque player
+
+
+journal de développement 
+30/09/2022 ->création du html et nommaage des classes et id
+ligne 21  a 24 index.html
+<!-- le faites de rajouter -- a la fin de la classe de cet maniére joueur--0 ça permet de selectionner la  classe  avec précision voir script.js  -->
+    <!--la classe joueur--actif va servir a définir quelle joueur est en train de jouer en va utiliser la propriété toggle avec le javascript pour qu'elle soit dans la section du joueur 2 quand le joueur 1 aura fini de jouer -->
+2/création du nouvelle branche 
+3/nommage des variables et des id
+4/rajout de la régle du jeu a écrire dans le html
+
+3/10/2022 fichier script.js
+création des variables pour manipuler le DOM
--- script.js
+++ script.js
@@ -1,40 +1,18 @@
 console.log('connecté');
-//initialisation des variables 
-//let lancer ;
-//let securiser ;
-//entrez le nom des participants
-// let player1 = prompt("Entrez le nom du joueur 1");
-// let player2 = prompt("Entrez le nom du joueur 2");
+//stockage des joueurs en
+const joueur01 = document.querySelector('.joueur--1');
+console.log(joueur01+'ligne 26');//le console.log est la por déboguer je le laisse dans la cadre de l'évaluation .
+const joueur02 = document.querySelector('.joueur--2');
+console.log(joueur01+'ligne 52');
 
-// stocker les éléments
-//nouvelle partie
-let newGame = document.querySelector('.newGame');
-console.log(newGame);
-function clickOnNewGame () {
-    console.log('c\'est le bouton nouvelle partie');
-}
+//score définitif 
+const scoreDéfinitif01 = document.querySelector('#scoreDéfinitif--1');//score définitif du joueur 1 
+console.log(scoreDéfinitif01+'ligne 34');
+const scoreDéfinitif02 = document.querySelector('#scoreDéfinitif--2');//score définitif du joueur 2
+console.log(scoreDéfinitif02+'ligne 34');
 
-
-//lancer 
-//bouton lancer
- let rollDice = document.querySelector('.rollDice');
- console.log(rollDice);
- function lancerLeDes(){
-    console.log('c\'est le bouton lancer')
- }
-
-
- //bouton securiser
- let hound = document.querySelector('.hound');
- console.log(hound);
- function securiserLeResultat(){
-    console.log('c\'est le bouton securiser')
- }
-
- //form
-
- let formOne = document.querySelector('.formPlayerOne');
- console.log(formOne);
-
- let formTwo = document.querySelector('.formPlayerTwo')
- console.log(formTwo)
+//score provisoire
+const scoreProvisoire01 = document.querySelector('#scoreProvisoire--1');
+console.log(scoreProvisoire01+'ligne 37')
+const scoreProvisoire02 = document.querySelector('#scoreProvisoire--2 ');
+console.log(scoreProvisoire01+'ligne 64');
